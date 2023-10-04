f3e7058c7d696a2f6a6b992f1809f2f00a3725a0
Tomas Szabo <bady.tomas33@gmail.com>
2023-10-04 22:42:28 +0100

    Initial commit

---
+++ Assets/Resources/Scripts/FlyBehaviour.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class FlyBehaviour : MonoBehaviour
+{
+    [SerializeField] private float velocity = 1.5f;
+    [SerializeField] private float rotationSpeed = 10f;
+    private Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        if (Mouse.current.leftButton.wasPressedThisFrame)
+        {
+            rb.velocity = Vector2.up * velocity;
+        }
+    }
+    private void FixedUpdate()
+    {
+        transform.rotation = Quaternion.Euler(0,0, rb.velocity.y * rotationSpeed);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision) 
+    {
+       GameManager.instance.GameOver(); 
+    }
+
+}
